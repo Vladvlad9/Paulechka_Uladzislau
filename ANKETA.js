2bda1f19ef2cc075a0f232d6676a282c674bcc09
Vlad Paulechka <vlad9.ru@gmail.com>
2023-07-12 14:45:08 +0300

    добали ;

--- ANKETA.js
+++ ANKETA.js
@@ -3,21 +3,21 @@ while(fio == '' || fio == null){
     fio = prompt("Введите ФИО");
 }
 
-let age = prompt("Введите ваш возраст")
+let age = prompt("Введите ваш возраст");
 while(isNaN(age) || age == null || age == ""){
-    age = Number(prompt("Введите ваш возраст"))
+    age = Number(prompt("Введите ваш возраст"));
 }
 
-const sex = confirm("Ваш пол - мужской?")
+const sex = confirm("Ваш пол - мужской?");
 
 function get_sex(sex, age){
     let pension;
     if(sex){
-        pension = age > 63 ? "\nвы на пенсии: ДА" : "\nвы на пенсии: Нет";
+        pension = (age > 63) ? "\nвы на пенсии: ДА" : "\nвы на пенсии: Нет";
         return `Мужской ${pension}`;
     }
     else
-        pension = age > 58 ? "\nвы на пенсии: ДА" : "\nвы на пенсии: Нет";
+        pension = (age > 58) ? "\nвы на пенсии: ДА" : "\nвы на пенсии: Нет";
         return `Женский ${pension}`;
 }
 
